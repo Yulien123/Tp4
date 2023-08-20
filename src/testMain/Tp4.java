1ff5a1137240a1b359bf402cae615edc58e98984
Julio Flores <juliofloresth@gmail.com>
2023-08-20 17:44:26 -0300

    Agrego Main Final

---
+++ src/testMain/Tp4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testMain;
+
+import entity.Convertidor;
+
+/**
+ *
+ * @author julan
+ */
+public class Tp4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Convertidor().setVisible(true);
+    }
+}
